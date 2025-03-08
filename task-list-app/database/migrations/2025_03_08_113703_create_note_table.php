a19c4656fa5f22a468d3bdde23a1cd001a0bbada
Lauren Jackson <81902255+Twigg99@users.noreply.github.com>
2025-03-08 11:45:10 +0000

    New migrations created for sidebar and notes
    Using a foreign key relationship in the notes table.

---
+++ task-list-app/database/migrations/2025_03_08_113703_create_note_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('notes', function (Blueprint $table) {
+            $table->id();
+            $table->string('note_title');
+            $table->string('note_image_path')->nullable();
+            $table->text('note_body');
+            $table->foreignId('sidebar_link_id')->constrained('sidebar')->onDelete('cascade'); // Ensure foreign key relationship
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('notes');
+    }
+};
