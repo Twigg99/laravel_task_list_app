589cb658e9e2aee75cc89695649fab2d97fc17b4
Lauren Jackson <81902255+Twigg99@users.noreply.github.com>
2025-03-17 10:40:30 +0000

    Creating basic skeleton

--- frontend-task-list-app/src/App.vue
+++ frontend-task-list-app/src/App.vue
@@ -1,14 +1,23 @@
 <script setup>
-
+import Sidebar from './components/Sidebar.vue';
+import { RouterView } from 'vue-router';
 </script>
 
 <template>
-  <div>
-    
+  <div class="app-container">
+    <Sidebar />
+    <main>
+      <RouterView /> 
+    </main>
   </div>
-  
 </template>
 
 <style scoped>
-
+.app-container {
+  display: flex;
+}
+main {
+  flex: 1;
+  padding: 20px;
+}
 </style>
