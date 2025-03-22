9fab9921036138868e0bb257915772398e64119b
Lauren Jackson <81902255+Twigg99@users.noreply.github.com>
2025-03-22 10:29:41 +0000

    starting the sidebar skeleton

---
+++ frontend-task-list-app/src/components/Sidebar.vue
@@ -0,0 +1,34 @@
+<template>
+  <aside class="sidebar">
+    <nav>
+      <ul>
+        <li><router-link to="/">All Tasks</router-link></li>
+
+      </ul>
+    </nav>
+  </aside>
+</template>
+<script setup>
+</script>
+<style>
+.sidebar {
+    width: 250px;
+    height: 100%;
+    background: #f4f4f4;
+    display: flex;
+    justify-content: center;
+    position: fixed;
+    top: 0;
+    left: 0;
+  
+}
+nav ul {
+  list-style: none;
+  padding: 0;
+  width: 200px;
+  display: flex;
+  justify-content: center;
+}
+nav ul li {
+  margin-bottom: 10px;
+}</style>
