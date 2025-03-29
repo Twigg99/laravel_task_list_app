fdacb3b6dc3f4d6ecaf8a80e210de2fac037229c
Lauren Jackson <81902255+Twigg99@users.noreply.github.com>
2025-03-29 21:17:54 +0000

    new router.js added

--- frontend-task-list-app/src/router.js
+++ frontend-task-list-app/src/router.js
@@ -1,8 +1,10 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import Dashboard from './components/Dashboard.vue';
+import Holiday from './components/Holiday.vue';
 
 const routes = [
   { path: '/', component: Dashboard },
+  { path: '/holiday', component: Holiday },
 ];
 
 const router = createRouter({
