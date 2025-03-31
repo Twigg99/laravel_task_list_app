342e0bab2a3c8c8b0b16191ed5429bc4eb632da4
Lauren Jackson <81902255+Twigg99@users.noreply.github.com>
2025-03-31 20:47:04 +0100

    sidebar styling

--- frontend-task-list-app/src/components/Sidebar.vue
+++ frontend-task-list-app/src/components/Sidebar.vue
@@ -7,15 +7,15 @@
    
       
       <ul class="navigationList">
-        <li><router-link to="/">All Tasks</router-link></li>
-        <li><router-link to="/holiday">Holidays</router-link></li>
+        <li><router-link to="/" class="iconSidebar"><ClipboardList ></ClipboardList><span v-if="!isCollapsed">All Tasks</span><span v-if="!isCollapsed"><TrashIcon></TrashIcon></span></router-link></li>
+        <li><router-link to="/holiday" class="iconSidebar"><Plane></Plane><span v-if="!isCollapsed">Holidays</span><span v-if="!isCollapsed"><TrashIcon></TrashIcon></span></router-link></li>
       </ul>
       <button class="addSectionButton"><CirclePlus></CirclePlus><span v-if="!isCollapsed">Add New Section</span></button>
     </nav>
   </aside>
 </template>
 <script setup>
-import { Home, List, Plus, ChevronLeftCircle, CirclePlus } from "lucide-vue-next";
+import { ChevronLeftCircle, CirclePlus, ClipboardList, TrashIcon, Plane } from "lucide-vue-next";
 import { ref } from "vue";
 
 const isCollapsed = ref(false);
@@ -28,4 +28,12 @@ const collapseSidebar = () => {
 
 </script>
 <style>
+
+.iconSidebar {
+  display: flex;
+  width: 100%;
+  justify-content: space-evenly;
+}
+
+
 </style>
