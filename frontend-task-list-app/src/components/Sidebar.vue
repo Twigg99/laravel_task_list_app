e4794fcd499713bcfebc2f3dd49234df380b3bba
Lauren Jackson <81902255+Twigg99@users.noreply.github.com>
2025-04-20 10:57:37 +0100

    Sidebar hover trash icon

--- frontend-task-list-app/src/components/Sidebar.vue
+++ frontend-task-list-app/src/components/Sidebar.vue
@@ -2,13 +2,13 @@
   <aside class="sidebar"  :class="['sidebar', { collapsed: isCollapsed }]">
     <ChevronLeftCircle class="chevron-icon" @click="collapseSidebar" :class="['chevron-icon', { rotated: isCollapsed }]"/>
     <nav>
-      <h2>Organiser</h2>
-      <hr class="organiserSeparator">
+      <h2 v-if="!isCollapsed">Organiser</h2>
+      <hr v-if="!isCollapsed"  class="organiserSeparator">
    
       
       <ul class="navigationList">
-        <li><router-link to="/" class="iconSidebar"><ClipboardList ></ClipboardList><span v-if="!isCollapsed">All Tasks</span><span v-if="!isCollapsed"><TrashIcon></TrashIcon></span></router-link></li>
-        <li><router-link to="/holiday" class="iconSidebar"><Plane></Plane><span v-if="!isCollapsed">Holidays</span><span v-if="!isCollapsed"><TrashIcon></TrashIcon></span></router-link></li>
+        <li><router-link to="/" class="iconSidebar"><ClipboardList ></ClipboardList><span v-if="!isCollapsed">All Tasks</span><span><TrashIcon class="trash-icon" v-if="!isCollapsed"></TrashIcon></span> </router-link></li>
+        <li><router-link to="/holiday" class="iconSidebar"><Plane></Plane><span v-if="!isCollapsed">Holidays</span><span><TrashIcon class="trash-icon" v-if="!isCollapsed"></TrashIcon></span></router-link></li>
       </ul>
       <button class="addSectionButton"><CirclePlus></CirclePlus><span v-if="!isCollapsed">Add New Section</span></button>
     </nav>
@@ -35,5 +35,14 @@ const collapseSidebar = () => {
   justify-content: space-evenly;
 }
 
+/* Hide the trash icon by default */
+.trash-icon {
+  opacity: 0;
+  transition: opacity 0.2s ease-in-out;
+}
 
+/* Show trash icon on hover of its parent li */
+nav ul li:hover .trash-icon {
+  opacity: 1;
+}
 </style>
